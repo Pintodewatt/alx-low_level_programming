0cbaa1d2e86f994813bbd773168c60d992206afc
Solomon Oteng <Otengsolomon6@gmail.com>
2023-08-13 13:14:39 -0700

    combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,6 +13,7 @@ int main(void)
 		{
 		if (m > n)
 					{
+
 					putchar(n);
 					putchar(m);
 					if (n != 56 || m != 57)
@@ -24,5 +25,5 @@ int main(void)
 					}
 					}
 					putchar('\n');
-					return (0)
+					return (0);
 }

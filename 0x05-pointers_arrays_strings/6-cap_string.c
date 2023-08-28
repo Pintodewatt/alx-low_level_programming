b74e509e21e6b3d24e6eced5302b18e624fef4a2
Solomon Oteng <Otengsolomon6@gmail.com>
2023-08-28 03:42:21 -0700

    6cap

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes the first character of a word
+ * @str: string to capitalize
+ * Return: returns the capitalized string
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			if (index == 0 ||
+				str[index - 1] == ' ' || str[index - 1] == '\t' ||
+				str[index - 1] == '\n' || str[index - 1] == ',' ||
+				str[index - 1] == ';' || str[index - 1] == '.' ||
+				str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' ||
+				str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			{
+				str[index] -= 32;
+			}
+		}
+		index++;
+	}
